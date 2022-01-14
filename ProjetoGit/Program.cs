6bfd7fa9155836731efd8c55bcb130bb206fe8f1
Edinei Alves <edineialves535@gmail.com>
2022-01-13 21:46:37 -0300

    Bem Aventuranca

---
+++ ProjetoGit/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ProjetoGit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Bem Aventurança");
+        }
+    }
+}
